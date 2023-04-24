c0f6044bc0198d1586d940d0a0093d31573fb516
asaRicky <derrickomondi208@gmail.com>
2023-04-24 14:28:40 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,19 @@
 import React, { useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import ExpenseList from "./ExpenseList";
 
 function App (props) {
     const [expenses, setExpenses] = useState([]);
     
-    function addExpenses(expenses)
+    function addExpenses(expense) {
+        setExpenses([...expenses, expense]);
+    }
+    return (
+        <div>
+            <ExpenseForm onSubmit={addExpenses} />
+            <ExpenseList onSubmit={expenses} />
+        </div>
+    );
 }
+
+export default App;
